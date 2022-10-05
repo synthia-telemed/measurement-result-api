6c781dca640ea45a03ee75598cd5d4c6b1913e87
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-10-05 20:52:35 +0700

    add: schema validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ConfigModule, ConfigService } from '@nestjs/config'
+import * as Joi from 'joi'
 import { AppController } from './app.controller'
 import { BloodPressureModule } from './blood-pressure/blood-pressure.module'
 
 @Module({
 	imports: [
-		ConfigModule.forRoot({ isGlobal: true }),
+		ConfigModule.forRoot({
+			isGlobal: true,
+			validationSchema: Joi.object({ MONGODB_CONNECTION_STRING: Joi.string().required() }),
+		}),
 		MongooseModule.forRootAsync({
 			imports: [ConfigModule],
 			useFactory: async (configService: ConfigService) => ({
