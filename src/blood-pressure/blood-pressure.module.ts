b0e9a22a13f97498c14b853781bf8165f3f823cb
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-11-11 09:29:07 +0700

    wired up

--- src/blood-pressure/blood-pressure.module.ts
+++ src/blood-pressure/blood-pressure.module.ts
@@ -4,10 +4,12 @@ import { BloodPressureController } from './blood-pressure.controller'
 import { BloodPressure, BloodPressureSchema } from './schema/blood-pressure.schema'
 import { BloodPressureService } from './blood-pressure.service'
 import { HospitalModule } from 'src/hospital/hospital.module'
+import { PulseController } from './pulse.controller'
+import { PulseService } from './pulse.service'
 
 @Module({
 	imports: [MongooseModule.forFeature([{ name: BloodPressure.name, schema: BloodPressureSchema }]), HospitalModule],
-	controllers: [BloodPressureController],
-	providers: [BloodPressureService],
+	controllers: [BloodPressureController, PulseController],
+	providers: [BloodPressureService, PulseService],
 })
 export class BloodPressureModule {}
